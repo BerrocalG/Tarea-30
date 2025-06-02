fb9dfe90773a9bd0fe611dda5fd4e30e09995a1e
BerrocalG <guadalupe.berrocal@usil.pe>
2025-06-02 17:32:28 -0500

    ejercicio base clase mario

---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+//ejercicio 2: tarea cancion mario bros
+void config_timer(void){
+  TCCR0A|=(1<<WGM01);
+  TCCR0B|=(1<<CS02);
+}
+
+void DO(void){
+  OCR0A=118;
+  TCNT0=0;
+  while(!(TIFR0&(1<<OCF0A)));
+  TIFR0|=(1<<OCF0A);
+}
+
+void MI(void){
+  OCR0A=94;
+  TCNT0=0;
+  while(!(TIFR0&(1<<OCF0A)));
+  TIFR0|=(1<<OCF0A);
+}
+
+void SOL(void){
+  OCR0A=79;
+  TCNT0=0;
+  while(!(TIFR0&(1<<OCF0A)));
+  TIFR0|=(1<<OCF0A);
+}
+
+void SOL_(void){
+  OCR0A=159;
+  TCNT0=0;
+  while(!(TIFR0&(1<<OCF0A)));
+  TIFR0|=(1<<OCF0A);
+}
+
+int main(void){
+
+  config_timer();
+  DDRB|=(1<<PB0);
+  PORTB&=~(1<<PB0);
+
+  while(1){
+    for(int i=0;i<83;i++){
+      PORTB^=0x01;
+      MI();
+    }
+    PORTB&=~(0x01);
+
+    _delay_ms(1);
+
+    for(int i=0;i<83;i++){
+      PORTB^=0x01;
+      MI();
+    }
+    PORTB&=~(0x01);
+
+    _delay_ms(150);
+
+    for(int i=0;i<83;i++){
+      PORTB^=0x01;
+      MI();
+    }
+
+    _delay_ms(250);
+
+    for(int i=0;i<65;i++){
+      PORTB^=0x01;
+      DO();
+    }
+    PORTB&=~(0x01);
+
+    _delay_ms(1);
+
+    for(int i=0;i<83;i++){
+      PORTB^=0x01;
+      MI();
+    }
+    PORTB&=~(0x01);
+
+    _delay_ms(150);
+
+    for(int i=0;i<392;i++){
+      PORTB^=0x01;
+      SOL();
+    }
+    PORTB&=~(0x01);
+
+    _delay_ms(1);
+
+    for(int i=0;i<392;i++){
+      PORTB^=0x01;
+      SOL_();
+    }
+    PORTB&=~(0x01);
+
+    _delay_ms(1000);
+  }
+  return 0;
+}
+  
